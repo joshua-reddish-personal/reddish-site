6ba12c4081a5696d60836fc196acbba18fe012b3
joshua-reddish-personal <joshreddish7@gmail.com>
2025-02-01 20:16:35 -0600

    feat: closes #12 Enhance MediaTable component: add active link shading based on current pathname

--- personal-site/app/components/mediaTable.tsx
+++ personal-site/app/components/mediaTable.tsx
@@ -1,26 +1,29 @@
-// components/MediaTable.tsx
+"use client"
+
 import Link from 'next/link'
+import { usePathname } from 'next/navigation'
 import { ReactNode } from 'react'
 
 type MediaTableProps = {
     children?: ReactNode
 }
-// TO DO: Add shading to the current link
+
 export default function MediaTable({ children }: MediaTableProps) {
+    const pathname = usePathname()
     return (
         <div>
             <nav className="bg-gray-800 p-4">
-                <div className="container mx-auto">
-                    <Link href="/reddish-reviews/movies" className="text-white mr-4">
+                <div className="container mx-auto flex space-x-4">
+                    <Link href="/reddish-reviews/movies" className={`text-white px-4 py-2 rounded ${pathname === '/reddish-reviews/movies' ? 'bg-gray-700' : ''}`}>
                         Movies
                     </Link>
-                    <Link href="/reddish-reviews/books" className="text-white mr-4">
+                    <Link href="/reddish-reviews/books" className={`text-white px-4 py-2 rounded ${pathname === '/reddish-reviews/books' ? 'bg-gray-700' : ''}`}>
                         Books
                     </Link>
-                    <Link href="/reddish-reviews/tv-shows" className="text-white mr-4">
+                    <Link href="/reddish-reviews/tv-shows" className={`text-white px-4 py-2 rounded ${pathname === '/reddish-reviews/tv-shows' ? 'bg-gray-700' : ''}`}>
                         TV Shows
                     </Link>
-                    <Link href="/reddish-reviews/video-games" className="text-white mr-4">
+                    <Link href="/reddish-reviews/video-games" className={`text-white px-4 py-2 rounded ${pathname === '/reddish-reviews/video-games' ? 'bg-gray-700' : ''}`}>
                         Video Games
                     </Link>
                 </div>
